300a2dfece8571c211f18377adad2af4a7d3cc20
Jakob Rath <git@jakobrath.eu>
2019-09-10 15:04:29 +0200

    Implement parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,25 @@
 module Main where
 
+-- base
+import System.Environment (getArgs)
+import System.Exit (exitFailure, exitSuccess)
+
+-- bytestring
+import qualified Data.ByteString as B
+
+-- zsh-history
+import Zsh.History.Parser
+
+
 main :: IO ()
-main = putStrLn "Hello World!"
+main = do
+  [inputFile] <- getArgs
+  putStrLn $ "Reading file: " <> inputFile
+  input <- B.readFile inputFile
+  case parseHistory input of
+    Left err -> do
+      putStrLn $ "Error: " <> err
+      exitFailure
+    Right history -> do
+      _ <- traverse print history
+      exitSuccess
