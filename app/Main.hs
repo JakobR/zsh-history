690c50d2bae4d4a830d0b3cfe624eba1b3efb9b5
Jakob Rath <git@jakobrath.eu>
2019-09-11 10:30:40 +0200

    purify

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,8 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main
+  ( main
+  ) where
 
 -- base
 import Control.Monad
@@ -9,9 +13,9 @@ import System.IO (hPrint, hPutStrLn, stderr, stdin)
 import Data.Text (Text)
 import qualified Data.Text.IO as Text.IO
 import qualified Data.Text.Lazy.IO as Text.Lazy.IO
--- import Data.Text.Lazy.Builder (Builder)
+import Data.Text.Lazy.Builder (Builder)
 import qualified Data.Text.Lazy.Builder as Builder
--- import qualified Data.Text.Lazy.Builder.Int as Builder
+import qualified Data.Text.Lazy.Builder.Int as Builder
 
 -- time
 import Data.Time.Clock.POSIX
@@ -39,25 +43,27 @@ mainFormat :: Bool -> FormatOptions -> IO ()
 mainFormat _isDebug options = do
   inputData <- readInput (input options)
   inputHistory <- abortOnLeft (parseHistory inputData)
-  printHistory (format options) inputHistory
-
-
-printHistory :: OutputFormat -> [Entry] -> IO ()
-printHistory TextOutputFormat history = do
   tz <- getCurrentTimeZone
-  forM_ history (putStrLn . formatEntryText tz)
-printHistory ZshOutputFormat history = do
-  let rendered = mconcat (renderEntry <$> history)
-  Text.Lazy.IO.putStr (Builder.toLazyText rendered)
-printHistory JSONOutputFormat _history = do
-  abortWithError "not yet implemented"
+  let output = cmdFormat options tz inputHistory
+  Text.Lazy.IO.putStr (Builder.toLazyText output)
 
 
-formatEntryText :: TimeZone -> Entry -> String
-formatEntryText tz e =
-  formatTime (error "bug: no locale set") "%Y-%m-%d %H:%M:%S" (entryLocalTime e)
-  <> " (" <> show (duration e) <> "s) "
-  <> show (Zsh.History.command e)
+cmdFormat :: Foldable f => FormatOptions -> TimeZone -> f Entry -> Builder
+cmdFormat options tz inputHistory =
+  renderHistory (format options) tz inputHistory
+
+
+renderHistory :: Foldable f => OutputFormat -> TimeZone -> f Entry -> Builder
+renderHistory TextOutputFormat tz history = foldMap (renderEntryText tz) history
+renderHistory ZshOutputFormat  _  history = foldMap renderEntry history
+renderHistory JSONOutputFormat _ _history = error "not yet implemented"
+
+
+renderEntryText :: TimeZone -> Entry -> Builder
+renderEntryText tz e =
+  Builder.fromString (formatTime (error "bug: no locale set") "%Y-%m-%d %H:%M:%S" (entryLocalTime e))
+  <> " (" <> Builder.decimal (duration e) <> "s) "
+  <> Builder.fromText (Zsh.History.command e)
   where
     entryLocalTime = utcToZonedTime tz . posixSecondsToUTCTime . fromIntegral . timestamp
 
